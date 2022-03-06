71d770ac1faef3c9223b0e6e91380b88f1cab3c9
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-06 20:34:16 +0100

    funcion precedentes

--- topologica.py
+++ topologica.py
@@ -2,6 +2,16 @@ tareas = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 restricciones = [(1, 2), (2, 4), (3, 4), (5, 8), (7, 9), (9, 4), (3, 1)]
 completadas = []
 
+#esta funcion mira que tareas tienen restricciones y cuantos son, y los añade a una lista auxiliar
+def precedentes(t):
+    retorno = []
+    for j in range(len(restricciones)):
+        res = restricciones[j]
+        if t == res[1]:
+            retorno.append(res[0])
+    return retorno
+
+
 
 #codigo principal
 while len(completadas) < len(tareas):
